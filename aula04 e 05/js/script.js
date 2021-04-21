16a5add69ca1bb4bd322c9847e0613a27ead98bf
Malu Melo <mmaalluu12@gmail.com>
2021-04-21 15:01:37 -0300

    Removing the static on HTML and transforming into JS responsive

--- aula04 e 05/js/script.js
+++ aula04 e 05/js/script.js
@@ -1,7 +1,23 @@
-const title = "Harry Potter"
-const url = "http:\\imagem.com"
-
 const movie = {
     title: "Harry Potter",
     url: "http:\\imagem.com",
 };
+
+const figure = document.createElement('figcaption');
+const img = document.createElement('img');
+const title = document.createElement('figcaption');
+const listBanners = document.querySelector(".list-banners");
+
+figure.classList.add("wrapper-banner");
+
+img.src = 'img/banner2.png';
+img.alt = 'Banner of the Anime';
+img.classList.add('main-banner');
+
+title.textContent = "Nome do Anime";
+title.classList.add('main-caption');
+
+figure.insertAdjacentElement('beforeend',img);
+figure.insertAdjacentElement('beforeend',title);
+
+listBanners.insertAdjacentElement('beforeend',figure);
